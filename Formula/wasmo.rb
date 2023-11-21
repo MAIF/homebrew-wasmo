cc2033cf8f8a1ca71a046cf24e09d398e99babdc
Etienne ANNE <etienne.anne@icloud.com>
2023-11-21 15:39:37 +0100

    Update wasmo.rb

--- Formula/wasmo.rb
+++ Formula/wasmo.rb
@@ -4,9 +4,9 @@
 class Espanso < Formula
   desc "WASM Binaries Builder"
   homepage "https://github.com/MAIF/wasmo/tree/main/cli"
-  url "https://github.com/MAIF/wasmo/archive/refs/tags/0.1.2.tar.gz"
-  sha256 "b1040e50bcb6498249c5e521e29e701117425df5bcc44b17a06d520ec1244a92"
-  version "0.1.2"
+  url "https://github.com/MAIF/wasmo/archive/refs/tags/0.1.6.tar.gz"
+  sha256 "ab8b9915ab620c473418dfc9a795670ce602649ef3c69f639cd84c8e5d56286b"
+  version "0.1.6"
 
   def install
     bin.install "wasmo"
