34521351df239f75e09b43a0be54f1dc19f77817
Etienne ANNE <etienne.anne@icloud.com>
2024-06-26 08:53:38 +0200

    Update wasmo.rb

--- Formula/wasmo.rb
+++ Formula/wasmo.rb
@@ -5,9 +5,9 @@ class Wasmo < Formula
   desc "WASM Binaries Builder"
   homepage "https://github.com/MAIF/wasmo/tree/main/cli"
   license any_of: ["MIT", "0BSD"]
-  url "https://github.com/MAIF/wasmo/archive/refs/tags/1.0.15.tar.gz"
-  sha256 "77b2286c61baf0f4416c5abe31ce6a75e6633f1afc60cff26782a754250e9f94"
-  version "1.0.15"
+  url "https://github.com/MAIF/wasmo/archive/refs/tags/1.3.0.tar.gz"
+  sha256 "8d84a6ad619e52829fab7ff7f38000c16c96868d668af658328c9320dc9e2f6b"
+  version "1.3.0"
 
   depends_on "rust" => :build
 
